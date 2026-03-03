f3a6640580f50994cdab1f5a55a9549385127915
altynaysailau <altynayx@gmail.com>
2026-03-03 16:30:39 +0500

    lab06

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,32 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './product-list/product-list'; // Импорт нового компонента
-import { categories, products as initialProducts } from './data';
-import { Category, Product } from './models';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ProductListComponent],
+  imports: [CommonModule, RouterModule, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class AppComponent {
-  categories: Category[] = categories;
-  productList: Product[] = [...initialProducts]; // Храним общее состояние здесь
-  selectedCategory: Category | null = null;
-
-  selectCategory(category: Category) {
-    this.selectedCategory = category;
-  }
-
-  // Получаем только товары выбранной категории
-  get filteredProducts() {
-    return this.productList.filter(p => p.categoryId === this.selectedCategory?.id);
-  }
-
-  // Метод для удаления товара из общего списка
-  handleDelete(productId: number) {
-    this.productList = this.productList.filter(p => p.id !== productId);
-  }
+  title = 'album-browser';
 }
